2ec56d2cac38bbcab434e96fc09e3b54f72d81f5
Ahmed Dahya <a.dahya@live.com>
2017-12-30 14:46:42 +0300

    Adding Daemon capability to watchdog for TSSPM file Placing startup codes in a function

--- Start-Server.py
+++ Start-Server.py
@@ -1,20 +1,25 @@
 import sys, time
 from WatchDog.WatchDog import DB_watcher
 
+
+def start_TSSPM_Watchdog():
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            w.start()
+        elif 'stop' == sys.argv[1]:
+            w.stop()
+        elif 'restart' == sys.argv[1]:
+            w.restart()
+        else:
+            print("Unknown command")
+            sys.exit(2)
+        sys.exit(0)
+
+    else:
+        print("usage: %s start|stop|restart" % sys.argv[0])
+        sys.exit(2)
+
+
 w = DB_watcher(folder='/var/Security/WatchDog/SCRDB/')
 
-if len(sys.argv) == 2:
-    if 'start' == sys.argv[1]:
-        w.start()
-    elif 'stop' == sys.argv[1]:
-        w.stop()
-    elif 'restart' == sys.argv[1]:
-        w.restart()
-    else:
-        print("Unknown command")
-        sys.exit(2)
-    sys.exit(0)
-
-else:
-    print("usage: %s start|stop|restart" % sys.argv[0])
-    sys.exit(2)
+start_TSSPM_Watchdog()
